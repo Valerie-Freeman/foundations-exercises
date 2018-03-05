c10db45da835fb19bc36635c815c37130e7c7ee3
Valerie Freeman <valerah7@gmail.com>
2018-03-05 14:44:00 -0600

    cleaned up code

--- flag.js
+++ flag.js
@@ -4,12 +4,12 @@ const repl = require('repl');
 const chalk = require('chalk');
 
 // Set variables
-const star = chalk.white.bgBlue.bold('*');
-const blue = chalk.bgBlue(' ');
+const stars7 = chalk.white.bgBlue.bold(' * * * * * * * ');
+const stars6 = chalk.white.bgBlue.bold('  * * * * * *  ')
 const red = chalk.bgRed('                                   ');
 const white = chalk.bgWhite('                                   ');
-const blue_redStripe = `${blue}${star}${blue}${star}${blue}${star}${blue}${star}${blue}${star}${blue}${star}${blue}${star}${blue}${red}`;
-const blue_whiteStripe = `${blue}${blue}${star}${blue}${star}${blue}${star}${blue}${star}${blue}${star}${blue}${star}${blue}${blue}${white}`;
+const blue_redStripe = `${stars7}${red}`;
+const blue_whiteStripe = `${stars6}${white}`;
 const redStripe = chalk.bgRed('                                                  ');
 const whiteStripe = chalk.bgWhite('                                                  ');
 
